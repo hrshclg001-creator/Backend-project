76f3649ec9ed0407aa1e0dce7e536e59ad724521
harshmehra <hrshclg001@gmail.com>
2026-07-15 17:27:19 +0530

    Fix subscription model typo and enhance user controller with channel profile and watch history features

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,11 @@
+import mongoose from "mongoose";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js"; // Fixed case
 import { uploadOnCLoudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import { Subscription } from "./../models/subscription.model";
 // register user function
 const registerUser = asyncHandler(async (req, res) => {
   // 1. Getting user details
@@ -282,8 +284,8 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
   if (!avatarLocalPath) {
     throw new ApiError(400, " Avatar file is missing ");
   }
-  const avatar = await uploadOnCLoudinary(avatarLocalPath);
-  if (!avatar.url) {
+  const avatar = uploadOnCLoudinary(avatarLocalPath);
+  if (!avatar?.url) {
     throw new ApiError(400, " Avatar url is missing ");
   }
   const user = await User.findByIdAndUpdate(
@@ -298,8 +300,9 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     }
   ).select("-password");
 
-  return res.status(200)
-  .json(new ApiResponse(200, user, "Avatar Image updated Successfully."))
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar Image updated Successfully."));
 });
 
 const updateUserCoverImage = asyncHandler(async (req, res) => {
@@ -308,7 +311,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Error in coverImageLocalPath");
   }
   const coverImage = await uploadOnCLoudinary(coverImageLocalPath);
-  if (!coverImage.url) {
+  if (!coverImage?.url) {
     throw new ApiError(400, "Error while uploading cover Image on cloudinary");
   }
   const user = await User.findByIdAndUpdate(
@@ -322,8 +325,125 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
       new: true,
     }
   ).select("-password");
-  return res.status(200)
-  .json(new ApiResponse(200,user,"Cover Image updated successfully."))
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover Image updated successfully."));
+});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing.");
+  }
+  //agrregate pipelines code
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      //for number of Subscriber of a channel
+      $lookup: {
+        from: "subscription",
+        localField: "_id",
+        foreignField: "channel",
+        as: "Subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "Subscription",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "SubscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$SubscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, "$subscribers.subscriber"],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel doesnot exists.");
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfuly.")
+    );
+});
+
+//pipeline for getting watch history of user.
+const getWatchHistory = asyncHandler(async (req, res) => {
+  //Step 2: Get Current User
+  //   Important Note on ObjectIds:
+  //   When you access req.user._id, you get a string representation
+  //   MongoDB internally uses ObjectId type
+  // Mongoose handles conversion automatically in most cases
+  //In aggregation pipelines, you must manually convert using new mongoose.Types.ObjectId()
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from : "users",
+              localField : "owner",
+              foreignField : "_id",
+              as : "owner",
+			  pipeline: [
+				{
+					$project : {
+						fullname: 1,
+						username : 1,
+						avatar : 1
+					}
+				}
+			  ]
+            },
+          },
+		  
+        ],
+      },
+    },
+  ]);
 });
 
 export {
@@ -336,4 +456,6 @@ export {
   updateUserDetail,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 };
